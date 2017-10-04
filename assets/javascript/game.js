8ecafefc6950ccb89c8e6103e5cf531f31479a80
Jennifer Bono <jennb24@gmail.com>
2017-10-04 11:46:53 -0500

    new files and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,19 @@
 var words = [
-	"BASKETBALL", 
-	"FOOTBALL",
-	"SOCCER",
-	"BASKET",
-	"TOUCHDOWN",
-	"GOAL",
-	"TENNIS",
-	"COACH",
-	"HELMET",
-	"BASEBALL",
-	"PLAYER",
-	"BATTER",
+	"basketball", 
+	"football",
+	"soccer",
+	"basket",
+	"touchdown",
+	"goal",
+	"tennis",
+	"coach",
+	"helmet",
+	"baseball",
+	"player",
+	"batter",
 	];
-var answer = [];
 var validKeys = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
 var wins = 0;
 var guesses = 10;
 var allChosenLetters = [];
@@ -23,46 +23,54 @@ var word = words[Math.floor(Math.random() * words.length)];
 var remainingLetters = word.length;
 
 //Set up Answer Array
+var answer = [];
 for (var i = 0; i < word.length; i++) {
 	answer[i] = "_";
+	window.onload = updateHTML;
 }
 
+function updateHTML() { // Functions should come after all variables are defined
+	document.getElementById("winCount").innerHTML = wins;
+	document.getElementById("displayWord").innerHTML = answer.join (' ');
+	document.getElementById("guessesLeft").innerHTML = guesses;
+	document.getElementById("guessedLetters").innerHTML = allChosenLetters.join(', ');
+}
+document.onload = updateHTML // When the page has fully loaded, call this function!
+
 document.onkeyup = function(event) {
+  	if (guesses == 0) { return; }
 	// for (var i = 0; i < word.length; i++) {
 		// words.push("_");
 		//get the letter they entered
-	var letterGuess = String.fromCharCode(event.keyCode).toUpperCase();
+	var letterGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		//make sure they haven't guessed it before 
 	if (allChosenLetters.indexOf(letterGuess) > -1) {
 		//already guessed
 		alert("You've already guessed this letter.");
-		
-	}
-	else { //not already guessed
-		allChosenLetters.push(letterGuess);
+	} else { //not already guessed
+		for (var i = 0; i < validKeys.length; i++){
+			if (validKeys[i] == letterGuess) {
+                allChosenLetters.push(letterGuess);
 
-		if (word.indexOf(letterGuess) > -1) {
-			//letter guessed is in word
-			for(var i = 0; i < word.length; i++) {
-				word.charAt(i) == letterGuess;
-			}
+                if (word.indexOf(letterGuess) > -1) {
+                    //letter guessed is in word
+                    for (var i = 0; i < word.length; i++) {
+                        if (word.charAt(i) == letterGuess) {
+                            answer[i] = letterGuess;
+                        }
+                    }
+                } else {
+                  	//letter guessed is not in word
+                 	guesses--;
+                }
+              	updateHTML();
+                if (guesses == 0) {
+                   	alert("You've run out of guesses! Game Over."); 
+                }
+              
+              	return;
+            }
 		}
-		else { //letter guessed is not in word
-
-		}
+        alert("Please choose a letter between a and z.");
 	}
-
-
-		 
-		
-		//if it's right replacing underscore with letter 
-		//always put letter with entered letters 
-		//alaways check to see if they win or lose
-
-		//only if it's not in chosen letters will it take away a guess
-		
 }
-
-
-		// if its a win increase win total 
-			// put up image of sport word
